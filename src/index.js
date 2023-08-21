d0f154fb63235cf3611fbb8dbc988e461b57867c
xzMatheuSx <matheusagusth@gmail.com>
2023-08-21 17:32:33 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+
+const listenPort = 3001;
+const app = express()
+app.use(cors({
+    origin:'*'
+}),express.json());
+
+app.listen(listenPort, () => {
+    console.log('API rodando na porta 3001 on http://localhost:'+ listenPort);
+});
+
+
+
+app.get('/', (req, res) => {
+    res.send('Listando usuários');
+});
+
+app.get('/usuario/:id', (req, res) => {
+    const id = req.params.id;
+    res.send('Usuário' + id);
+    
+});
+
+app.post('/usuario', (req, res) => {
+    const data = req.body;
+    res.json(data);
+});
+
+app.put('/usuario/:id', (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+    res.json({ id, ...data });
+});
+
+app.delete('/usuario/:id', (req, res) => {
+    const id = req.params.id;
+    res.send('Excluindo usuário' + id);
+});
+
+
