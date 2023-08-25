5f13caa24b934426d30b70fe4bc6eb346828cca1
xzMatheuSx <matheusaugusth@gmail.com>
2023-08-25 18:55:20 -0300

    Refatorando código e adicionando try catch

--- src/index.js
+++ src/index.js
@@ -13,29 +13,49 @@ app.listen(listenPort, () => {
 
 
 
-app.get('/', (req, res) => res.status(200).send('Listando usuários'));
-
-app.get('/usuario/:id', (req, res) => {
-    const id = req.params.id;
-    res.send('Usuário:' + id);
-    
-});
-
-app.post('/usuario', (req, res) => {
-    const infos = req.body;
-    
-    res.json(infos);
-});
-
-app.put('/usuario/:id', (req, res) => {
-    const id = req.params.id;
-    const data = req.body;
-    res.json({ id, ...data });
-});
-
-app.delete('/usuario/:id', (req, res) => {
-    const id = req.params.id;
-    res.send('Excluindo usuário' + id);
-});
-
+app.get('/', (req, res) => {
+    try {
+      res.status(200).send('Listando usuários');
+    } catch (err) {
+      res.status(404).json({ error: 'Erro ao listar usuários.' });
+    }
+  });
+  
+  app.get('/usuario/:id', (req, res) => {
+    try {
+      const id = req.params.id;
+      res.send('Usuário: ' + id);
+    } catch (err) {
+      res.status(404).json({ error: 'Erro ao buscar usuário.' });
+    }
+  });
+  
+  app.post('/usuario', (req, res) => {
+    try {
+      const infoUsuario = req.body;
+      res.json(infoUsuario);
+    } catch (err) {
+      res.status(500).json({ error: 'Erro ao criar usuário.' });
+    }
+  });
+  
+  app.put('/usuario/:id', (req, res) => {
+    try {
+      const id = req.params.id;
+      const data = req.body;
+      res.json({ id, ...data });
+    } catch (err) {
+      res.status(500).json({ error: 'Erro ao atualizar usuário.' });
+    }
+  });
+  
+  app.delete('/usuario/:id', (req, res) => {
+    try {
+      const id = req.params.id;
+      res.send('Excluindo usuário ' + id);
+    } catch (err) {
+      res.status(500).json({ error: 'Erro ao excluir usuário.' });
+    }
+  });
+  
 
